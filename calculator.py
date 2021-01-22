195312b02a07da08428629fadb039241c6616cf3
hajjjat2004 <53589537+hajjjat2004@users.noreply.github.com>
2021-01-22 20:49:11 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -12,6 +12,5 @@ def subtract(first_term, second_term):
 def multiply(first_term, second_term):
     return first_term * second_term
 
-
 def division(first_term, second_term):
     return first_term / second_term)
